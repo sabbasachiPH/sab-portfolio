e06fb0a7f14640628e207edf99d2cc13b129e307
sabbasachiPH <rentustat02@gmail.com>
2020-05-27 14:41:00 +0600

    problem fixing

--- src/Components/Contact/ContactForm.js
+++ src/Components/Contact/ContactForm.js
@@ -10,67 +10,84 @@ const ContactForm = () => {
     document.getElementById("myForm").style.display = "none";
   }
   return (
-    <form
-      name="contactForm"
-      action="POST"
-      data-netlify="true"
-      className="contactForm"
-    >
-      <fieldset>
-        <legend>GET IN TOUCH</legend>
-        <div class="form-group">
-          <label for="fname">Name:</label>
-          <input
-            className="form-control"
-            type="text"
-            id="name"
-            name="name"
-            placeholder="Full Name"
-          />
-        </div>
-        <div class="form-group">
-          <label for="lname">Phone:</label>
-          <input
-            className="form-control"
-            type="tel"
-            minlength="10"
-            required
-            id="phone"
-            name="phone"
-            placeholder="Cell No"
-          />
-        </div>
-        <div class="form-group">
-          <label for="email">Email:</label>
-          <input
-            className="form-control"
-            type="email"
-            id="email"
-            name="email"
-            required
-            placeholder="First Name"
-          />
-        </div>
-        <div class="form-group">
-          <label for="message">Message:</label>
-          <textarea
-            className="form-control"
-            type="textarea"
-            id="message"
-            name="message"
-            placeholder="Write your message"
-          />
-        </div>
-        <div class="form-group">
-          <div data-netlify-recaptcha="true"></div>
-          <button className="btn btn-secondary" type="submit">
-            Send Message
-          </button>
-        </div>
-        <br />
-        {/* <input className="btn btn-secondary" type="submit" value="Send to Us" /> */}
-      </fieldset>
-    </form>
+    <>
+      <form action="POST" data-netlify="true" className="contactForm">
+        <fieldset>
+          <legend>GET IN TOUCH</legend>
+          <div class="form-group">
+            <label for="fname">Name:</label>
+            <input
+              className="form-control"
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Full Name"
+            />
+          </div>
+          <div class="form-group">
+            <label for="lname">Phone:</label>
+            <input
+              className="form-control"
+              type="tel"
+              minlength="10"
+              required
+              id="phone"
+              name="phone"
+              placeholder="Cell No"
+            />
+          </div>
+          <div class="form-group">
+            <label for="email">Email:</label>
+            <input
+              className="form-control"
+              type="email"
+              id="email"
+              name="email"
+              required
+              placeholder="First Name"
+            />
+          </div>
+          <div class="form-group">
+            <label for="message">Message:</label>
+            <textarea
+              className="form-control"
+              type="textarea"
+              id="message"
+              name="message"
+              placeholder="Write your message"
+            />
+          </div>
+          <div class="form-group">
+            <div data-netlify-recaptcha="true"></div>
+            <button className="btn btn-secondary" type="submit">
+              Send Message
+            </button>
+          </div>
+          <br />
+          {/* <input className="btn btn-secondary" type="submit" value="Send to Us" /> */}
+        </fieldset>
+      </form>
+      <form name="contact" netlify className="contactForm">
+        <p class="form-group">
+          <label>
+            Name <input type="text" name="name" />
+          </label>
+        </p>
+        <p class="form-group">
+          <label>
+            Email <input type="email" name="email" />
+          </label>
+        </p>
+        <p class="form-group">
+          <label>
+            Message <textarea type="text" name="message" />
+          </label>
+        </p>
+        <p class="form-group">
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </>
   );
 };
 // return (
